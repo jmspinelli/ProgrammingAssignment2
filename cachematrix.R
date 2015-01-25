60da71898f393e111b1a9be74c0a074b2033413a
Joseph Spinelli <joseph.spinelli@gmail.com>
2015-01-25 02:11:22 -0500

    Lexical Scoping Script for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  inversematrix <- function(solve) m<<- solve
+  getmatrix <- function() m
+  list(set=set, get=get, inversematrix=inversematrix, getmatrix=getmatrix)
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("Getting Cached Inverse Matrix")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$inversematrix(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
+
